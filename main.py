3d54b6e6d9da90717f3e1118255052bd1ba478c9
Andrey Kolomatskiy <andrey@kolomatskiy.ru>
2022-12-01 15:59:03 +0100

    download population file if it is absent

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import sys
 
 from geometry import cut_area
-from tools import check_requirements
+from tools import check_requirements, download_population, unpack_file
 
 if __name__ == '__main__':
     if len(sys.argv) < 5:
         print("Use: python population_cutter.py top left bottom right")
         # sample
         # 7048591.26497715 9306376.98870119 7040405.72953559 9327904.30238216
+        # 45.27334396 19.79934486 45.24239696 19.85313726
         sys.exit()
 
     # load coordinates and cast it to float
@@ -41,11 +42,33 @@ if __name__ == '__main__':
     # define data file)
     gpkg_file = os.path.join('resources', 'kontur_population.gpkg')
     if not os.path.exists(gpkg_file):
-        print("FATAL ERROR! File with population is absent")
-        sys.exit()
+        print("ERROR! File with population is absent")
+        answer = input("Do you want to download it? y/n\n")
+        if answer.lower() == 'y' or answer.lower() == 'yes':
+            try:
+                population_file = 'kontur_population_20220630.gpkg.gz'
+                if not os.path.exists(os.path.join('resources', population_file)):
+                    os.makedirs('resources', exist_ok=True)
+                    print("Downloading population data... It may take some time")
+                    local_filename = download_population(population_file)
+            except Exception as e:
+                print('Error occurred while downloading population', e)
+                sys.exit()
 
+            try:
+                print("Unarchive population data... It may take some time")
+                if unpack_file(local_filename, gpkg_file):
+                    print("Population file data loaded successfully")
+                else:
+                    raise Exception()
+            except Exception as e:
+                print('Error occurred while unpacking population', e)
+                sys.exit()
+        else:
+            sys.exit()
     # cut population
     try:
+        print("Cutting population inside defined area... It may take some time")
         cut_area(gpkg_file, output_file, top, left, bottom, right)
     except Exception as e:
         print('Error occurred while cutting population', e)
--- tools.py
+++ tools.py
@@ -1,5 +1,10 @@
+import gzip
+import shutil
 import subprocess
 import sys
+import requests
+import os
+import tqdm
 
 
 def check_requirements():
@@ -8,3 +13,25 @@ def check_requirements():
     except Exception as e:
         print('Error occurred while installing requirements: {}'.format(e))
         sys.exit()
+
+
+def download_population(population_file):
+    url = 'https://geodata-eu-central-1-kontur-public.s3.amazonaws.com/kontur_datasets/{}'.format(population_file)
+    local_filename = os.path.join('resources', population_file)
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            pbar = tqdm.tqdm(unit="MB", position=0, leave=True)
+            chunk_size = 8192
+            for chunk in r.iter_content(chunk_size):
+                f.write(chunk)
+                pbar.update(round(chunk_size/1000,))
+            pbar.close()
+    return local_filename
+
+
+def unpack_file(input_file, gpkg_file):
+    with gzip.open(input_file, 'rb') as file:
+        with open(gpkg_file, 'wb') as out_file:
+            shutil.copyfileobj(file, out_file)
+    return os.path.exists(gpkg_file)
